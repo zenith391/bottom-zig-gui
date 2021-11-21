880f5f0e05486e8ea1e3b285a5a061ea47fdd9da
zenith391 <39484230+zenith391@users.noreply.github.com>
2021-11-21 14:55:40 +0100

    Start encoding

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,52 @@
 const std = @import("std");
 const zgt = @import("zgt");
+const BottomEncoder = @import("encoder.zig").BottomEncoder;
 pub usingnamespace zgt.cross_platform;
+
+pub fn encode(button: *zgt.Button_Impl) !void {
+    // If we received the event, this means we're parented, so it's safe to assert
+    const root = button.getRoot().?;
+
+    // Get 'encode-text' and cast it to a TextField
+    const input = root.get("encode-text").?.as(zgt.TextField_Impl);
+    std.log.info("Encode {s}", .{ input.getText() });
+
+    // Same cast, but with 'decode-text'
+    const output = root.get("decode-text").?.as(zgt.TextField_Impl);
+
+    const allocator = zgt.internal.scratch_allocator;
+    const encoded = try BottomEncoder.encode(input.getText(), allocator);
+    defer allocator.free(encoded);
+
+    output.setText(encoded);
+}
 
 pub fn main() !void {
     try zgt.backend.init();
 
     var window = try zgt.Window.init();
     try window.set(
-        zgt.Button(.{ .label = "Test" })
+        zgt.Column(.{}, .{
+            zgt.Label(.{ .text = "Bottom encoder and decoder" }),
+            zgt.Column(.{}, .{
+                zgt.Row(.{}, .{
+                    zgt.Label(.{ .text = "Input text" }),
+                    zgt.TextField(.{ })
+                        .setName("encode-text"),
+                }),
+                zgt.Row(.{}, .{
+                    zgt.Label(.{ .text = "Output text" }),
+                    zgt.TextField(.{ })
+                        .setName("decode-text"),
+                }),
+            }),
+            zgt.Row(.{}, .{
+                zgt.Button(.{ .label = "Encode", .onclick = encode }),
+                zgt.Button(.{ .label = "Decode" }),
+            })
+        })
     );
-    
+
     window.resize(800, 600);
     window.show();
 
